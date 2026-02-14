29c330cef7fa87ca774982945e1ef759c6bc22b2
waldos2704 <waldosiahaan2704@gmail.com>
2026-02-14 10:42:59 +0700

    update

--- answer.py
+++ answer.py
@@ -1,6 +1,5 @@
-target_question = input()
+import pandas as pd
 
-<<<<<<< HEAD
 # Baca data
 df = pd.read_excel("data_kuesioner.xlsx")
 
@@ -205,55 +204,3 @@ elif target_question == "q13":
     p_neg = round(negatif/total*100,1)
 
     print(f"positif={positif}:{p_pos}|netral={netral}:{p_net}|negatif={negatif}:{p_neg}")
-=======
-if target_question == "q1":
-    # Skala paling banyak dipilih
-    print("S|1176|61.2")
-
-elif target_question == "q2":
-    # Skala paling sedikit dipilih
-    print("STS|4|0.2")
-
-elif target_question == "q3":
-    # SS terbanyak
-    print("Q9|21|18.6")
-
-elif target_question == "q4":
-    # S terbanyak
-    print("Q16|75|66.4")
-
-elif target_question == "q5":
-    # CS terbanyak
-    print("Q2|36|31.9")
-
-elif target_question == "q6":
-    # CTS terbanyak
-    print("Q9|8|7.1")
-
-elif target_question == "q7":
-    # TS terbanyak
-    print("Q12|8|2.7")
-
-elif target_question == "q8":
-    # STS terbanyak
-    print("Q12|8|2.7")
-
-elif target_question == "q9":
-    print("Q1:0.9|Q2:0.9|Q9:0.9|Q11:0.9")
-
-elif target_question == "q10":
-    # Skor rata-rata keseluruhan
-    print("4.80")
-
-elif target_question == "q11":
-    # Skor rata-rata tertinggi
-    print("Q5:4.95")
-
-elif target_question == "q12":
-    # Skor rata-rata terendah
-    print("Q12:4.59")
-
-elif target_question == "q13":
-    # Kategori positif, netral, negatif
-    print("positif=1396:72.7|netral=471:24.5|negatif=54:2.8")
->>>>>>> d9d902fd0db33f5b5e900034ce9a130bf48a2e55
